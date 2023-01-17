e55e1b73345e0405f799b5a74291fb3b557d2799
Georgvit <ifreyd@mail.ru>
2023-01-17 21:11:42 +0300

    solutionOne

--- Solid1Srp1/src/solid/Main.java
+++ Solid1Srp1/src/solid/Main.java
@@ -2,11 +2,12 @@ package solid;
 
 import solid.srp.Point;
 import solid.srp.Square;
+import solid.srp.SquareZoom;
 
 public class Main {
     public static void main(String[] args) {
-        Square square = new Square(new Point(1,1), 5);
+        Square square = new Square(new Point(1,1), 9);
         System.out.printf("Площадь фигуры: %d \n", square.getArea());
-        square.draw();
+        new SquareZoom(square).draw(square);
     }
 }
--- Solid1Srp1/src/solid/srp/SquareZoom.java
+++ Solid1Srp1/src/solid/srp/SquareZoom.java
@@ -1,4 +1,26 @@
 package solid.srp;
 
 public class SquareZoom {
+    private Square square;
+
+    public SquareZoom(Square square) {
+        this.square = square;
+    }
+
+    public void draw(Square square) {
+        for (int i = 0; i < square.getSide(); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 0; i < square.getSide(); i++) {
+            System.out.print("*");
+            for (int j = 1; j < square.getSide() - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        for (int i = 0; i < square.getSide(); i++) {
+            System.out.print("*");
+        }
+    }
 }
